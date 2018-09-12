639da6ef75d75dc8e3b31c79abd815ff2ce16845
Engin Öztürk <engin.ozturk@bilyoner.com>
2018-09-12 10:41:13 +0300

    mutations added

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://<DBUSER>:<DBPASS>@ds151012.mlab.com:51012/authdb';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
